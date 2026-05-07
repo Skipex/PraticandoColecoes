bd72c232127b455fe2e6e519665dd17527bdef54
Gustavo H. Coelho <gustavo@kobit.com.br>
2026-05-07 10:32:53 -0300

    Listas e conjuntos

--- Program.cs
+++ Program.cs
@@ -1,4 +1,237 @@
 ﻿
+// DICIONÁRIOS
+// Guardam uma estrutura de chave => valor
+
+TrabalhandoComDictionary();
+
+void TrabalhandoComDictionary()
+{
+    Dictionary<int, string> produtos = new Dictionary<int, string>
+    {
+        { 4587963, "Teclado" },
+        { 3374561, "Cadeira gamer" },
+        { 2456987, "Notebook" },
+        { 6356984, "Teclado" },
+        { 8647921, "Mouse" }
+    };
+
+    produtos.Add(789456, "Placa de Vídeo");
+
+}
+
+
+
+
+
+//HASHSET - CONJUNTO
+// Dados que não tem repetição de elementos
+// Não é possível pegar pelo Índice, apenas pelo valor
+
+//TrabalhandoComHashSet();
+//GamesScore();
+//MensagemSecretaLista();
+//SorteioLista();
+//ListaInventario();
+
+void ListaInventario()
+{
+    // Declare duas listas distintas de produtos.
+    // Adicione todos os elementos da lista secundária na lista principal.
+    // Exiba a lista principal com todos os produtos.
+
+    List<string> produtosPrincipal = new List<string> { "Mouse", "Teclado", "Monitor", "Joystick"};  
+    List<string> produtosSecundaria = new List<string> { "Headset", "Webcam", "Smartphone", "Mousepad" };
+
+    produtosPrincipal.AddRange(produtosSecundaria);
+
+    System.Console.WriteLine(string.Join(", ", produtosPrincipal));
+
+}
+
+void SorteioLista()
+{
+    // Declare uma lista de nomes.
+    // Organize a lista em ordem alfabética.
+    // Inverta a ordem da lista.
+    // Remova o primeiro elemento e o adicione novamente na lista.
+    // Agora verifique quem está no índice 3 da lista e exiba o nome no console.
+    
+    List<string> nomes = new List<string> { "Ana", "Carlos", "Mariana", "João", "Lúcia" };
+
+    System.Console.WriteLine("\nLista original: \n");
+
+    /*
+    foreach( string nome in nomes)
+    {
+        System.Console.Write($"{nome}, ");
+    }
+    System.Console.Write("\n");*/
+    System.Console.WriteLine(string.Join(", ", nomes));
+
+    nomes.Sort();
+
+    System.Console.WriteLine("\nLista ordenada: \n");
+
+    /*
+    foreach( string nome in nomes)
+    {
+        System.Console.Write($"{nome}, ");
+    }
+    System.Console.Write("\n");
+    */
+    System.Console.WriteLine(string.Join(", ", nomes));
+
+    nomes.Reverse();
+
+    System.Console.WriteLine("\nLista reversa: \n");
+
+    /*
+    foreach( string nome in nomes)
+    {
+        System.Console.Write($"{nome}, ");
+    }
+    System.Console.Write("\n");
+    */
+    System.Console.WriteLine(string.Join(", ", nomes));
+
+    var nomeRemovido = nomes[0];
+    nomes.RemoveAt(0);
+    nomes.Add(nomeRemovido);
+
+    System.Console.WriteLine("\nLista após remover o primeiro e adicioná-lo ao fim: \n");
+
+    /*
+    foreach( string nome in nomes)
+    {
+        System.Console.Write($"{nome}, ");
+    }
+    System.Console.Write("\n");
+    */
+    System.Console.WriteLine(string.Join(", ", nomes));
+
+    System.Console.WriteLine($"\nO nome sorteado é: {nomes[3]}");
+
+
+
+
+}
+
+void MensagemSecretaLista()
+{
+    List<char> mensagem = new List<char>
+    {
+        'o', 'd', 'n', 'u', 'm', ' ', 'á', 'l', 'o'
+    };
+
+    mensagem.Reverse();
+
+    foreach (char letra in mensagem)
+    {
+        System.Console.WriteLine(letra);
+    }
+}
+
+void GamesScore()
+{
+
+    HashSet<int> scores = new HashSet<int>();
+
+    scores.Add(150);
+    scores.Add(90);
+    scores.Add(200);
+    scores.Add(120);
+    scores.Add(150);
+    scores.Add(80);
+    scores.Add(180);
+    scores.Add(200);
+
+    List<int> scoresOrdenados = new List<int>(scores);
+
+    scoresOrdenados.Sort();
+
+    foreach (int score in scoresOrdenados)
+    {
+        System.Console.WriteLine(score);
+    }
+}
+
+void TrabalhandoComHashSet()
+{
+    HashSet<string> setFuncionarios = new HashSet<string>
+    {
+        "Ana", "Paula", "Marcos", "José"
+    };
+
+    setFuncionarios.Remove("Marcos");
+
+    setFuncionarios.Add("João");
+    setFuncionarios.Add("Ana");
+
+    foreach (string funcionario in setFuncionarios)
+    {
+        System.Console.WriteLine(funcionario);
+    }
+
+
+}
+
+
+
+
+
+
+
+/****************** FUNÇÕES DE LISTAS ********************/
+
+void TrabalhandoComListas()
+{
+    // LISTAS
+    List<string> nomes = new List<string>
+    {
+        "Ana", "Paula", "Marcos", "José"
+    };
+
+    List<string> listaFuncionariosExp2 = new List<string>();
+
+    listaFuncionariosExp2.Add("Ana");
+    listaFuncionariosExp2.Add("Paula");
+    listaFuncionariosExp2.Add("João");
+    listaFuncionariosExp2.Add("Marcelo");
+    listaFuncionariosExp2.Add("Thaís");
+
+    System.Console.WriteLine("LISTAS\n\n");
+
+    System.Console.WriteLine(listaFuncionariosExp2[2]);
+
+    int indice = listaFuncionariosExp2.IndexOf("Marcelo");
+
+    System.Console.WriteLine($"Índice do Marcelo: {indice}");
+
+    System.Console.WriteLine("\nOrdenação\n");
+
+    listaFuncionariosExp2.Sort();
+
+    foreach (string funcionario in listaFuncionariosExp2)
+    {
+        System.Console.WriteLine(funcionario);
+    }
+
+    listaFuncionariosExp2.Add("Iasmin");
+    listaFuncionariosExp2.Add("Joana");
+    listaFuncionariosExp2.Remove("João");
+
+
+    listaFuncionariosExp2.Sort();
+
+    System.Console.WriteLine("\nLista com outros nomes:\n");
+
+    foreach (string funcionario in listaFuncionariosExp2)
+    {
+        System.Console.WriteLine(funcionario);
+    }
+}
+
+/****************** FUNÇÕES DE ARRAYS ********************/
 
 void DecifraCodigo()
 {
@@ -23,7 +256,7 @@ void OrdenaArray()
 
     Array.Sort(nomes);
 
-    System.Console.WriteLine($"O terceiro nome da lista é: {nomes[2]}");    
+    System.Console.WriteLine($"O terceiro nome da lista é: {nomes[2]}");
 }
 
 void SomaDoacoes()
